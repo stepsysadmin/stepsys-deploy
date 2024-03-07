53c2a6573b67631fb43a44d4a0ba801a730b3997
stepsysadmin <155295986+stepsysadmin@users.noreply.github.com>
2024-03-07 18:30:57 +0100

    Add files via upload

---
+++ win-deploy.ps1
@@ -0,0 +1,79 @@
+# Preparations
+# This is required for Windows 10 IoT Enterprise LTSC
+####### 
+# Fix Windows Store
+wsreset -i
+# Run Asheroto's WinGet fixer script
+irm https://github.com/asheroto/winget-install/releases/latest/download/winget-install.ps1 | iex
+#### Apply registry tweaks that I deem essential
+### Customization
+# Verbose logon messages
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v VerboseStatus /t REG_DWORD /d 1 /f
+# Disable auto correct
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\TabletTip\1.7 /v EnableAutocorrection /t REG_DWORD /d 0 /f
+# Disable highlight misspelled words
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\TabletTip\1.7 /v EnableSpellchecking /t REG_DWORD /d 0 /f
+# Set device usage mode to GAMING!!!
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CloudExperienceHost\Intent\gaming /v Intent /t REG_DWORD /d 1 /f
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CloudExperienceHost\Intent\gaming /v Priority /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CloudExperienceHost\Intent\OffDeviceConsent /v accepted /t REG_DWORD /d 1 /f
+# Enable Win32 long paths
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem /v LongPathsEnabled /t REG_DWORD /d 1 /f
+# Add copy as path context menu entry
+reg add HKEY_CLASSES_ROOT\Allfilesystemobjects\shell\windows.copyaspath /v @ /t REG_SZ /d "Copy &as path" /f
+reg add HKEY_CLASSES_ROOT\Allfilesystemobjects\shell\windows.copyaspath /v Icon /t REG_SZ /d "imageres.dll,-5302" /f
+reg add HKEY_CLASSES_ROOT\Allfilesystemobjects\shell\windows.copyaspath /v InvokeCommandOnSelection /t REG_DWORD /d 1 /f
+reg add HKEY_CLASSES_ROOT\Allfilesystemobjects\shell\windows.copyaspath /v VerbHandler /t REG_SZ /d "{f3d06e7c-1e45-4a26-847e-f9fcdee59be0}" /f
+reg add HKEY_CLASSES_ROOT\Allfilesystemobjects\shell\windows.copyaspath /v VerbName /t REG_SZ /d "copyaspath" /f
+# Show file extensions by default on
+reg add HKEY_CLASSES_ROOT\AllFilesystemObjects\shell\Windows.ShowFileExtensions /v CommandStateSync /t REG_SZ /d "" /f
+reg add HKEY_CLASSES_ROOT\AllFilesystemObjects\shell\Windows.ShowFileExtensions /v Description /t REG_SZ /d "@shell32.dll,-37571" /f
+reg add HKEY_CLASSES_ROOT\AllFilesystemObjects\shell\Windows.ShowFileExtensions /v ExplorerCommandHandler /t REG_SZ /d "{4ac6c205-2853-4bf5-b47c-919a42a48a16}" /f
+reg add HKEY_CLASSES_ROOT\AllFilesystemObjects\shell\Windows.ShowFileExtensions /v MUIVerb /t REG_SZ /d "@shell32.dll,-37570" /f
+reg add HKEY_CLASSES_ROOT\Directory\Background\shell\Windows.ShowFileExtensions /v CommandStateSync /t REG_SZ /d "" /f
+reg add HKEY_CLASSES_ROOT\Directory\Background\shell\Windows.ShowFileExtensions /v Description /t REG_SZ /d "@shell32.dll,-37571" /f
+reg add HKEY_CLASSES_ROOT\Directory\Background\shell\Windows.ShowFileExtensions /v ExplorerCommandHandler /t REG_SZ /d "{4ac6c205-2853-4bf5-b47c-919a42a48a16}" /f
+reg add HKEY_CLASSES_ROOT\Directory\Background\shell\Windows.ShowFileExtensions /v MUIVerb /t REG_SZ /d "@shell32.dll,-37570" /f
+### Performance
+# Disable Hardware accelerated GPU scheduling because it causes stutters when watching YouTube and playing ETS2 simultaneously
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers /v HwSchMode /t REG_DWORD /d 1 /f
+# Optimizations for windowed games
+reg add HKEY_CURRENT_USER\Software\Microsoft\DirectX\GraphicsSettings /v SwapEffectUpgradeCache /t REG_DWORD /d 1 /f
+reg add HKEY_CURRENT_USER\Software\Microsoft\DirectX\UserGpuPreferences /v DirectXUserGlobalSettings /t REG_SZ /d "SwapEffectUpgradeEnable=1;" /f
+# Disable startup delay
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Serialize /v StartupDelayInMSec /t REG_DWORD /d 0 /f
+### Privacy
+# Disable app launch tracking
+reg add HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\EdgeUI /v DisableMFUTracking /t REG_DWORD /d 1 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EdgeUI /v DisableMFUTracking /t REG_DWORD /d 1 /f
+# Bye bye File History
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\FileHistory /v Disabled /t REG_DWORD /d 1 /f
+# Find my Device is no more
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Settings\FindMyDevice /v LocationSyncEnabled /t REG_DWORD /d 0 /f
+# Disable shared experiences
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System /v EnableCdp /t REG_DWORD /d 0 /f
+# Disable ads, reset advertising ID, disable suggestions and disable Windows timeline
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v SilentInstalledAppsEnabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v SystemPaneSuggestionsEnabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSyncProviderNotifications /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v SoftLandingEnabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v RotatingLockScreenEnabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v RotatingLockScreenOverlayEnabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v SubscribedContent-310093Enabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo /v Enabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v SilentInstalledAppsEnabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager /v SubscribedContent-310093Enabled /t REG_DWORD /d 0 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System /v EnableActivityFeed /t REG_DWORD /d 0 /f
+# Disable Potentially Unwanted Application reporting
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender /v PUAProtection /t REG_DWORD /d 0 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine /v MpEnablePus /t REG_DWORD /d 0 /f
+### Security
+# Enable Windows Defender Core Isolation (Hypervisor-Enforced Code Integrity)
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity /v Enabled /t REG_DWORD /d 1 /f
+# Disable Automatic Sample Sumbission in Windows Defender
+Set-MpPreference -SubmitSamplesConsent NeverSend​
+#######
+### Install Tools and useful stuff using WinGet
+## Currently includes the following applications:
+## 7-zip, Audacity, BCUninstaller, CPU-Z, Discord, Everything, FFmpeg, IrfanView, OneDrive, VSCode, NAPS2, GPU-Z, Windows Terminal, VLC, WizTree
+winget install -e --id 7zip.7zip;winget install -e --id Audacity.Audacity;winget install -e --id Klocman.BulkCrapUninstaller;winget install -e --id CPUID.CPU-Z;winget install -e --id Discord.Discord;winget install -e --id voidtools.Everything;winget install -e --id Gyan.FFmpeg;winget install -e --id IrfanSkiljan.IrfanView;winget install -e --id Microsoft.OneDrive;winget install -e --id Microsoft.VisualStudioCode;winget install -e --id Cyanfish.NAPS2;winget install -e --id TechPowerUp.GPU-Z;winget install -e --id Microsoft.WindowsTerminal;winget install -e --id VideoLAN.VLC;winget install -e --id AntibodySoftware.WizTree
